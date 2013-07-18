334c2f7c69e501cf6dbf3ae904fb79707618f7fa
ocozalp <ozalp.orhan@gmail.com>
2013-07-18 14:52:19 +0300

    new gmpe names

--- common/values.py
+++ common/values.py
@@ -1,9 +1,9 @@
 __author__ = 'orhan'
 
 
-gmpes = ['AB_2003_AttenRel', 'AS_1997_AttenRel', 'AS_2008_AttenRel', 'AW_2010_AttenRel', 'Abrahamson_2000_AttenRel',
-         'AkB_2010_AttenRel', 'BA_2008_AttenRel', 'BC_2004_AttenRel', 'BJF_1997_AttenRel', 'BS_2003_AttenRel',
-         'BW_1997_AttenRel', 'CB_2003_AttenRel', 'CB_2008_AttenRel', 'CF_2008_AttenRel', 'CS_2005_AttenRel',
-         'CY_2008_AttenRel', 'Campbell_1997_AttenRel', 'DahleEtAl_1995_AttenRel', 'Field_2000_AttenRel',
-         'GouletEtAl_2006_AttenRel', 'LL_2008_AttenRel', 'McVerryetal_2000_AttenRel', 'SEA_1999_AttenRel',
-         'SadighEtAl_1997_AttenRel', 'Sokolov_2008_AttenRel', 'YoungsEtAl_1997_AttenRel', 'ZhaoEtAl_2006_AttenRel']
+gmpes = ['AbrahamsonSilva2008', 'AkkarEtAl2013', 'AkkarBommer2010', 'AkkarCagnan2010', 'AtkinsonBoore2003SInter',
+         'AtkinsonBoore2003SSlab', 'AtkinsonBoore2006', 'BooreAtkinson2008', 'Campbell2003', 'Campbell2003SHARE',
+         'CauzziFaccioli2008', 'ChiouYoungs2008', 'FaccioliEtAl2010', 'LinLee2008SInter', 'LinLee2008SSlab',
+         'SadighEtAl1997', 'SiMidorikawa1999Asc', 'SiMidorikawa1999SInter', 'SiMidorikawaSSlab', 'ToroEtAl2002',
+         'ToroEtAl2002SHARE', 'YoungsEtAl1997SInter', 'YoungsEtAl1997SSlab', 'ZhaoEtAl2006Asc', 'ZhaoEtAl2006SInter',
+         'ZhaoEtAl2006SSlab']
