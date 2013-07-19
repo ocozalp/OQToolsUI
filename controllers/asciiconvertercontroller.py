c3eb898140a33e1dd927aa4591387bf547f0e414
ocozalp <ozalp.orhan@gmail.com>
2013-07-19 19:08:43 +0300

    initial version.

---
+++ controllers/asciiconvertercontroller.py
@@ -0,0 +1,18 @@
+__author__ = 'orhan'
+
+
+from xml.etree import ElementTree
+
+
+def convertHazardMapToAscii(sourceFile, targetFile):
+    tree = ElementTree.parse(sourceFile)
+
+    with open(targetFile, 'w') as f:
+        nodes = tree.findall('./oq:hazardMap/oq:node', {'oq': 'http://openquake.org/xmlns/nrml/0.4'})
+
+        f.write('Lon\tLat\tIML\n')
+
+        for node in nodes:
+            f.write(node.attrib['lon'] + '\t' + node.attrib['lat'] + '\t' + node.attrib['iml'] + '\n')
+
+        f.flush()
