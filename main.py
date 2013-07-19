256348bdaf5b7eaeeddc4d30376d6dc6010d7f34
ocozalp <ozalp.orhan@gmail.com>
2013-07-19 18:32:46 +0300

    removed sample methods

--- main.py
+++ main.py
@@ -1,15 +1,3 @@
-'''
-Entry point for openquake admin
-
-Created on Mar 28, 2013
-
-@author: orhan
-'''
-
-import shape_parser.shapefile as shp
-from converters.shapefileconverter import  ShapeFileConverter
-from controllers.convertercontroller import *
-
 import sys
 from main_window import MainWindow
 from PyQt4 import QtGui as gui
@@ -21,15 +9,6 @@ def main():
     mw.show()
     sys.exit(app.exec_())
 
-def deneme():
-    sf = shp.Reader('/home/orhan/Desktop/LogFolders/20130613/new_shape/EMME_AsModel_Branch01.dbf')
-
-    for field in sf.fields:
-        print field
-
-def deneme2():
-    convert('/home/orhan/Desktop/LogFolders/20130618/shape/EMME_AsModel_ver2_4_BRANCH02_adjusted.shp',
-            '/home/orhan/Desktop/asd.xml')
 
 if __name__ == '__main__':
     main()
