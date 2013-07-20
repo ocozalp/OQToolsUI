71d85a223eea018f484438cf50aaee0e4728f79c
ocozalp <ozalp.orhan@gmail.com>
2013-07-21 02:58:54 +0300

    added result file type option.

--- panels/asciiconverterwindow.py
+++ panels/asciiconverterwindow.py
@@ -1,7 +1,7 @@
 from widgets import BrowseFileText
 from panels import BaseWindow
-from PyQt4 import QtGui as gui
-from controllers.asciiconvertercontroller import convertHazardMapToAscii
+from PyQt4 import QtGui as gui, QtCore as core
+from controllers.asciiconvertercontroller import *
 
 
 class AsciiConverterWindow(BaseWindow):
@@ -17,6 +17,12 @@ class AsciiConverterWindow(BaseWindow):
         self.targetFile = BrowseFileText(self, isDir=False)
         self.targetFile.initGui(self, 'Target File Name', 10, 60)
 
+        self.outputFileType = gui.QComboBox(self)
+        self.outputFileType.addItem(core.QString('Hazard Map'))
+        self.outputFileType.addItem(core.QString('Hazard Curve'))
+        self.outputFileType.addItem(core.QString('UH Spectra'))
+        self.outputFileType.setGeometry(260, 100, 150, 30)
+
         converter = gui.QPushButton('Convert', self)
         converter.setGeometry(410, 150, 120, 40)
         converter.clicked.connect(self.__convertFile)
@@ -24,5 +30,14 @@ class AsciiConverterWindow(BaseWindow):
     def __convertFile(self):
         s = self.sourceFile.getSelectedFile()
         t = self.targetFile.getSelectedFile()
+        type = self.outputFileType.currentIndex()
+        func = None
 
-        self.callFunction(lambda: convertHazardMapToAscii(s, t), 'Conversion is successful')
+        if type == 0:
+            func = convertHazardMapToAscii
+        elif type == 1:
+            func = convertHazardCurveToAscii
+        elif type == 2:
+            func = convertUhSpectraToAscii
+
+        self.callFunction(lambda: func(s, t), 'Conversion is successful')
