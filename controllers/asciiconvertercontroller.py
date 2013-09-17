0326d34c1101056e9944a1cd1b44ddafa0f92125
ocozalp <ozalp.orhan@gmail.com>
2013-09-17 15:20:34 +0300

    logarithmic interpolation for hazard curves. hazard curve excel document format has been changed

--- controllers/asciiconvertercontroller.py
+++ controllers/asciiconvertercontroller.py
@@ -1,5 +1,5 @@
 from xml.etree import ElementTree
-
+from math import log, exp
 
 def convertHazardMapToAscii(sourceFile, targetFile):
     tree = ElementTree.parse(sourceFile)
@@ -15,21 +15,22 @@ def convertHazardMapToAscii(sourceFile, targetFile):
         f.flush()
 
 
-def convertHazardCurveToAscii(sourceFile, targetFile):
+def convertHazardCurveToAscii(sourceFile, targetFile, returnPeriods=[72, 101, 475, 975, 2475, 4975, 10000]):
     tree = ElementTree.parse(sourceFile)
 
     with open(targetFile, 'w') as f:
         xmlNamespaces = {'oq': 'http://openquake.org/xmlns/nrml/0.4', 'gml': 'http://www.opengis.net/gml'}
 
         imlNode = tree.find('./oq:hazardCurves/oq:IMLs', xmlNamespaces)
-        imlVals = imlNode.text.split(' ')
+        imlVals = [float(iml) for iml in imlNode.text.split(' ')]
 
         nodes = tree.findall('./oq:hazardCurves/oq:hazardCurve', xmlNamespaces)
 
         header = 'Lon\tLat'
-        for imlVal in imlVals:
+        for returnPeriod in returnPeriods:
             header += '\t'
-            header += imlVal
+            header += str(returnPeriod)
+
         header += '\n'
 
         f.write(header)
@@ -38,14 +39,41 @@ def convertHazardCurveToAscii(sourceFile, targetFile):
             coordinates = node.find('gml:Point/gml:pos', xmlNamespaces).text.split(' ')
             row = coordinates[0] + '\t' + coordinates[1]
 
-            poes = node.find('oq:poEs', xmlNamespaces).text.split(' ')
-            for poe in poes:
-                row += '\t' + poe
+            poes = [float(poe) for poe in node.find('oq:poEs', xmlNamespaces).text.split(' ')]
+
+            for returnPeriod in returnPeriods:
+                rrp = 1.0 / returnPeriod
+                res = 0
+
+                if imlVals[0] >= rrp:
+                    res = imlVals[0]
+                elif imlVals[len(imlVals)-1] <= rrp:
+                    res = imlVals[len(imlVals)-1]
+                else:
+                    imlInd = 0
+                    for i in xrange(1, len(poes)):
+                        if poes[i] > rrp > poes[i-1] or poes[i-1] > rrp > poes[i]:
+                            imlInd = i
+                            break
+                    x1 = c_log(imlVals[imlInd-1])
+                    x2 = c_log(imlVals[imlInd])
+                    y1 = c_log(poes[imlInd-1])
+                    y2 = c_log(poes[imlInd])
+                    y = log(rrp)
+
+                    res = exp(((y - y1) * (x2 - x1)) / (y2 - y1) + x1)
+
+                row += '\t' + str(res)
 
             f.write(row + '\n')
 
         f.flush()
 
+def c_log(val):
+    if val == 0:
+        return 0.0
+
+    return log(val)
 
 def convertUhSpectraToAscii(sourceFile, targetFile):
     tree = ElementTree.parse(sourceFile)
