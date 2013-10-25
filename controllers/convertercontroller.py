960cac27b6dffc8f7d1dd28a37a5a5c061f66e97
ocozalp <ozalp.orhan@gmail.com>
2013-10-25 22:24:03 +0300

    single source model

--- controllers/convertercontroller.py
+++ controllers/convertercontroller.py
@@ -7,20 +7,14 @@ import os
 
 
 def convert(sourceFileName, targetFileName, sourceType,
-            nameMappings={'A': 'aGRval', 'B': 'bGRval', 'ID': 'EMME_IDAS', 'NAME': 'CODE'}):
-    deserializedShps = convertShapeFileToNrml(sourceFileName, targetFileName, nameMappings, sourceType)
+            nameMappings={'A': 'AGRVAL', 'B': 'BGRVAL', 'ID': 'EMME_IDAS', 'NAME': 'EMME_IDAS'}):
+    deserializedShp = convertShapeFileToNrml(sourceFileName, targetFileName, nameMappings, sourceType)
 
-    fileName, ext = os.path.splitext(targetFileName)
-    sourceModels = list()
-    for i in xrange(len(deserializedShps)):
-        deserializedShp = deserializedShps[i][0]
-        shapeFileName = fileName + '_' + str(i) + ext
-        writer = SourceModelXMLWriter(shapeFileName)
-        writer.serialize(deserializedShp)
-        sourceModels.append((os.path.basename(shapeFileName), deserializedShps[i][1]))
+    writer = SourceModelXMLWriter(targetFileName)
+    writer.serialize(deserializedShp)
 
-    writeSourceModelTree(os.path.join(os.path.dirname(targetFileName), 'source_model_logic_tree.xml'), sourceModels)
-
+    target_path = os.path.join(os.path.dirname(targetFileName), 'source_model_logic_tree.xml')
+    writeSourceModelTree(target_path, os.path.basename(targetFileName))
 
 def convertShapeFileToNrml(sourceFileName, targetFileName, nameMappings, sourceType):
     ind = sourceFileName.rindex('.')
@@ -30,7 +24,7 @@ def convertShapeFileToNrml(sourceFileName, targetFileName, nameMappings, sourceT
     return s.parse(sourceType)
 
 
-def writeSourceModelTree(targetFilePath, sourceModels):
+def writeSourceModelTree(targetFilePath, sourceModel):
     nrml_node = ElementTree.Element('nrml')
     nrml_node.set('xmlns', 'http://openquake.org/xmlns/nrml/0.4')
 
@@ -44,15 +38,14 @@ def writeSourceModelTree(targetFilePath, sourceModels):
     logic_tree_branch_set_node.set('uncertaintyType', 'sourceModel')
     logic_tree_branch_set_node.set('branchSetID', 'bs1')
 
-    for i in xrange(len(sourceModels)):
-        logic_tree_branch_node = ElementTree.SubElement(logic_tree_branch_set_node, 'logicTreeBranch')
-        logic_tree_branch_node.set('branchID', 'b'+str(i+1))
+    logic_tree_branch_node = ElementTree.SubElement(logic_tree_branch_set_node, 'logicTreeBranch')
+    logic_tree_branch_node.set('branchID', 'b0')
 
-        uncertainty_mode_node = ElementTree.SubElement(logic_tree_branch_node, 'uncertaintyModel')
-        uncertainty_mode_node.text = '../../SourceModels/' + sourceModels[i][0]
+    uncertainty_mode_node = ElementTree.SubElement(logic_tree_branch_node, 'uncertaintyModel')
+    uncertainty_mode_node.text = '../../SourceModels/' + sourceModel
 
-        uncertainty_weight_node = ElementTree.SubElement(logic_tree_branch_node, 'uncertaintyWeight')
-        uncertainty_weight_node.text = str(sourceModels[i][1])
+    uncertainty_weight_node = ElementTree.SubElement(logic_tree_branch_node, 'uncertaintyWeight')
+    uncertainty_weight_node.text = "1.0"
 
     tree = ElementTree.ElementTree(nrml_node)
 
