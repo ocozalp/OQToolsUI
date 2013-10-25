9153c5656a3ff2fde2b9335e7a77666edf6263fe
orhan <ozalp.orhan@gmail.com>
2013-10-26 02:51:36 +0300

    metadata modifications for pypi

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 PY_MODULES = ['oqtools.main']
 
@@ -8,12 +8,14 @@ setup(
             "OQToolsUI = oqtools.main:main",
             ],
         },
+    install_requires=['openquake.hazardlib<=0.11.0', 'openquake.nrmllib<=0.4.5', 'pyshp'],
     name='OQToolsUI',
     version='1.0.0',
-    packages=['common', 'ui', 'converters', 'controllers', 'oqtools'],
+    packages=find_packages(),
+    package_data={'oqtools': ['intensityLevels.txt', 'job.ini.template']},
     url='http://github.com/ocozalp/OQToolsUI',
     license='Apache License Version 2.0',
-    author='ocozalp',
+    author='Orhan Can Ozalp',
     author_email='ozalp.orhan@gmail.com',
     description='UI for openquake'
 )
