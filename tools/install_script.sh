37971d61abafe002ae75e76146ad47fd905367eb
orhan <ozalp.orhan@gmail.com>
2013-10-26 02:53:37 +0300

    pip install lines removed

--- tools/install_script.sh
+++ tools/install_script.sh
@@ -5,28 +5,4 @@ sudo apt-get install liblapack3gf liblapack-doc liblapack-dev
 sudo apt-get install gfortran
 sudo apt-get install libgeos-c1
 
-sudo pip install numpy
-sudo pip install scipy
-sudo pip install pyshp
-
-git clone https://github.com/gem/oq-nrmllib.git
-git clone https://github.com/gem/oq-hazardlib.git
-git clone https://github.com/gem/oq-risklib.git
-
-cd oq-nrmllib
-sudo python setup.py clean
-
-sudo python setup.py install
-cd ../oq-hazardlib
-sudo python setup.py clean
-sudo python setup.py install
-cd ../oq-risklib
-sudo python setup.py clean
-sudo python setup.py install
-
-git clone https://github.com/ocozalp/OQToolsUI.git
-cd OQToolsUI
-sudo python setup.py clean
-sudo python setup.py install
-cd ..
-rm -rf oq-nrmllib hazardlib oq-risklib OQToolsUI
+sudo pip install OQToolsUI
